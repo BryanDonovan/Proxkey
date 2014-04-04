59bdb5136856f109b9311dece058d5783524d37d
Doron Segal <doron2402@gmail.com>
2014-04-04 10:25:48 -0700

    stable state

--- example/index.js
+++ example/index.js
@@ -1,18 +1,24 @@
-var Proxkey = require('proxkey');
+var Proxkey = require("proxkey");
 var RoutesConfiguration = {
-	configuration: {
-		host: 'localhost',
-		port: '9191'
+	"configuration": {
+		"host": "localhost",
+		"port": "9191"
 	},
-	routes : [
+	"routes" : [
 			{
-				request_url: "/a/b",
-				method: "POST",
-				params: {
+				"request_url": "/a/b",
+				"method": "POST",
+				"params": {
 					"failure": [
 						{
 							"key": "phone_num",
 							"value": "9876543210"
+						}
+					],
+					"something": [
+						{
+							"key": "phone_num",
+							"value": "8888888888"
 						}
 					],
 					"production": [
@@ -22,23 +28,32 @@ var RoutesConfiguration = {
 						}
 					]
 				},
-				response: {
-					"type": "application/json",
-					"type_failure": "applicaton/xml",
-					"data": "partners/test/test_doc.json",
-					"data_failure": "partners/test/test_doc.xml",
-					"data_production": {
+				"response": {
+					"something" :{
+						"type": "applicaton/xml",
+						"data": "partners/test/test_some_doc.xml",
+						"code": 200
+					},
+					"failure" : {
+						"type": "applicaton/xml",
+						"data": "partners/test/test_doc.xml",
+						"code": 200
+					},
+					"success" : {
+						"type": "application/json",
+						"data": "partners/test/test_doc.json",
+						"code": 200
+					},
+					"production": {
 						"host": "httpbin.org",
 						"path": "/post",
 						"port": 80
-					},
-					"code": 200,
-					"code_failure": 404
+					}
 				}
 			},{
-				request_url: "/a/c",
-				method: "POST",
-				params: {
+				"request_url": "/a/c",
+				"method": "POST",
+				"params": {
 					"failure": [
 						{
 							"key": "phone_num",
@@ -49,18 +64,22 @@ var RoutesConfiguration = {
 						}
 					]
 				},
-				response: {
-					"type": "application/json",
-					"type_failure": "applicaton/xml",
-					"data": "partners/test/test_doc.json",
-					"data_failure": "partners/test/test_doc.xml",
-					"code": 200,
-					"code_failure": 404
+				"response": {
+					"success" : {
+						"type": "application/json",
+						"data": "partners/test/test_doc.json",
+						"code": 200
+					},
+					"failure" : {
+						"type": "applicaton/xml",
+						"data": "partners/test/test_doc.xml",
+						"code": 404
+					}
 				}
 			},{
-				request_url: "/say/hi",
-				method: "POST",
-				params: {
+				"request_url": "/say/hi",
+				"method": "POST",
+				"params": {
 					"failure": [
 						{
 							"key": "name",
@@ -68,15 +87,19 @@ var RoutesConfiguration = {
 						}
 					]
 				},
-				response: {
-					"type": "application/json",
-					"type_failure": "applicaton/json",
-					"data": "{ hi: 'Doron' }",
-					"data_failure": "{ hi: 'ben' }",
-					"code": 200,
-					"code_failure": 409
+				"response": {
+					"success": {
+						"type": "application/json",
+						"data": "{ hi: 'ben'}",
+						"code": 200
+					},
+					"failure": {
+						"type": "application/json",
+						"data": "{ hi: 'Doron' }",
+						"code": 409
+					}
 				}
 			}
 		]
 	};
-Proxkey.Start(RoutesConfiguration);
+Proxkey.start(RoutesConfiguration);
